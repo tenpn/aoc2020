5b4bbd21dc88f5f0ad0bddc0f4026afb766d3117
Andrew Fray <andrew.fray@gmail.com>
2020-12-02 21:00:20 +0000

    day 1 star 2

--- day1.py
+++ day1.py
@@ -12,7 +12,20 @@ def find_summing_pair(values: List[int], total: int) -> (int, int):
                 return (values[i], values[j])
     return None
 
+def find_summing_triple(values: List[int], total:int) -> (int, int, int):
+    for i in range(len(values)):
+        for j in range(i+1, len(values)):
+            for k in range(j+1, len(values)):
+                if values[i]+values[j]+values[k] == total:
+                    return (values[i], values[j], values[k])
+    return None
+
 if __name__ == '__main__':
+    print ("first star:")
     input = read_input()
     (v1, v2) = find_summing_pair(input, 2020)
     print(v1, v2, v1*v2)
+    print ("second star:")
+    (v1, v2, v3) = find_summing_triple(input, 2020)
+    print(v1, v2, v3, v1*v2*v3)
+    
--- day1.tests.py
+++ day1.tests.py
@@ -1,18 +1,32 @@
 import unittest
 from day1 import *
 
-class TestDay1(unittest.TestCase):
+class TestFindSummingPair(unittest.TestCase):
     def test_GIVEN_only_entries_sum_to_total_THEN_return_two_entries(self):
-        (v1, v2) = find_summing_pair([1,1], 2)
-        self.assertEqual((v1, v2), (1, 1))
+        res = find_summing_pair([1,1], 2)
+        self.assertEqual(res, (1, 1))
 
     def test_GIVEN_sum_entries_somewhere_in_list_THEN_return_sum_entries(self):
-        (v1, v2) = find_summing_pair([5, 3, 1, 6, 1], 2)
-        self.assertEqual((v1, v2), (1, 1))
+        res = find_summing_pair([5, 3, 1, 6, 1], 2)
+        self.assertEqual(res, (1, 1))
 
     def test_GIVEN_empty_list_THEN_return_None(self):
-        found_values = find_summing_pair([4,4], 1)
+        found_values = find_summing_pair([], 1)
         self.assertIsNone(found_values)
+
+class TestFindSummingTriple(unittest.TestCase):
+    
+    def test_GIVEN_empty_list_THEN_return_None(self):
+        found_values = find_summing_triple([], 1)
+        self.assertIsNone(found_values)
+
+    def test_GIVEN_only_entries_sum_to_total_THEN_return_three_entries(self):
+        res = find_summing_triple([2,3,4], 9)
+        self.assertEqual(res, (2, 3, 4))
+
+    def test_GIVEN_sum_entries_somewhere_in_list_THEN_return_sum_entries(self):
+        res = find_summing_triple([5, 3, 1, 6, 1], 5)
+        self.assertEqual(res, (3, 1, 1))
 
 if __name__ == '__main__':
     unittest.main()
